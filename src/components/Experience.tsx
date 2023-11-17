c64439525906cb6938fea268ce49c8e57a1af8b0
Dnyaneshwar ghodse <dnyaneshwarghodse@analytic-edge.com>
2023-11-17 23:29:38 +0530

    exp

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,21 +2,39 @@ const Experience = () => {
   return (
     <section>
       <div className="flex flex-wrap overflow-x-auto space-x-4 p-4 my-8">
-        <p className="text-cyan-400">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias saepe
-          eligendi dolorem ipsum obcaecati natus rem non id, temporibus aperiam
-          explicabo distinctio blanditiis repellendus aliquid unde eaque
-          laudantium vitae! Perspiciatis. Lorem ipsum dolor sit amet
-          consectetur, adipisicing elit. Ea eveniet, quod explicabo dignissimos
-          quis blanditiis commodi velit. Error, ex. Modi blanditiis vitae
-          aliquid! Magni accusamus, corporis magnam neque atque similique? Lorem
-          ipsum dolor sit, amet consectetur adipisicing elit. Deserunt
-          asperiores soluta sit aliquid quae possimus id eos, magni fugiat
-          minima! Adipisci aliquam molestias earum, libero numquam odit iste
-          itaque eaque! Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit. Similique sapiente, recusandae explicabo repellat doloremque
-          animi beatae sint, laboriosam qui magnam corrupti at optio harum
-          facere vel possimus tempore, a expedita?
+        <p className="text-cyan-200">
+          <div className="tabs flex gap-3">
+            <button className="text-cyan-400 border-b-2 border-cyan-600 p-2 hover:text-cyan-300">
+              Analytic Edge
+            </button>
+            <button className="text-cyan-400 border-b-2 border-cyan-600 p-2 hover:text-cyan-300">
+              Dhwani
+            </button>
+          </div>
+          <span>2021-2023</span>
+          <ul className="list-disc">
+            <li>
+              Developed and maintained responsive web applications using
+              React.js, collaborating with design and backend teams to ensure
+              pixel- perfect implementations.
+            </li>
+            <li>
+              Implemented complex frontend logic, including state management,
+              resulting in improved performance and code maintainability.
+            </li>
+            <li>
+              Contributed to architectural decisions, ensuring scalability and
+              optimization of the application for a growing user base.
+            </li>
+            <li>
+              Collaborated with QA engineers to identify and resolve frontend-
+              related bugs, resulting in a 20% decrease in post-release issues.
+            </li>
+            <li>
+              Actively participated in daily stand-ups and sprint planning,
+              following agile methodologies to deliver features on time.
+            </li>
+          </ul>
         </p>
       </div>
     </section>
--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -8,10 +8,18 @@ import {
 const Social = () => {
   return (
     <div className="flex gap-4 p-4 text-teal-400 text-2xl">
-      <GithubOutlined />
-      <LinkedinOutlined />
-      <MailOutlined />
-      <InstagramOutlined />
+      <a href="https://github.com/dnyanesh-ghodse07">
+        <GithubOutlined />
+      </a>
+      <a href="">
+        <LinkedinOutlined />
+      </a>
+      <a href="">
+        <MailOutlined />
+      </a>
+      <a href="">
+        <InstagramOutlined />
+      </a>
     </div>
   );
 };
