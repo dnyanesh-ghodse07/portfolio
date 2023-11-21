0bd6f710a14375cc8ea7bcd70c5fe195742754e3
Dnyaneshwar ghodse <dnyaneshwarghodse@analytic-edge.com>
2023-11-22 00:44:05 +0530

    text fix

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -9,7 +9,7 @@ const SkillCard = (skill: Skill) => {
   return (
     <div className="flex justify-center items-center gap-2 border-slate-50 p-2">
       <img width={40} src={icon} alt="" />
-      <h3 className="text-xl">{title}</h3>
+      <h3 className="text-xl text-slate-800 dark:text-slate-200">{title}</h3>
     </div>
   );
 };
